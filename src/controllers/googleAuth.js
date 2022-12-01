fdb0c0d686269762a6f210328e220918f5b8a9b6
songa1 <typa2611@gmail.com>
2022-12-01 19:59:11 +0200

    google authetication

--- src/controllers/googleAuth.js
+++ src/controllers/googleAuth.js
@@ -23,27 +23,25 @@ passport.use(
     },
 
     async (request, accessToken, refreshToken, profile, done) => {
-      console.log(profile, 'this is the profile');
       const user = await User.findOne({ email: profile.emails[0].value });
-      console.log(user, 'this is the user here')
       if (user) {
         const userId = user.id;
-        return done(null, { 
-          email:user.email,
-          names:user.names,
-          firstSignIn:false
-         });
+        return done(null, {
+          email: user.email,
+          names: user.names,
+          firstSignIn: false,
+        });
       }
       const newUser = await User.create({
         email: profile.emails[0].value,
         names: profile.displayName,
         password: null,
       });
-   
+
       return done(null, {
         email: profile.emails[0].value,
         names: profile.displayName,
-        firstSignIn:true
+        firstSignIn: true,
       });
     }
   )
